c619438588d1eb73bc979ab631d70c1380f0c660
Hari Nale <harinale132@gmail.com>
2024-02-02 10:47:42 +0530

    cleanup

---
+++ hari/ExtendExample.java
@@ -0,0 +1,18 @@
+package com.example.demo.hari;
+
+import com.example.demo.practice.Student1;
+
+class Test extends Student1 {
+
+
+    public Test(String mobileNumber, String name, float percentage) {
+        super(mobileNumber, name, percentage);
+    }
+
+}
+public class ExtendExample {
+    public static void main(String[] args) {
+       // Test t = new Student1("","",1.7f);
+
+    }
+}
